b272c3dd9ec87e82098bb50d1e9650e2e32973df
aleksandrawk <wiolettakulinska@tlen.pl>
2022-02-06 17:09:49 +0100

    Built API icons

--- src/app.js
+++ src/app.js
@@ -31,6 +31,10 @@ function showTemp(response) {
     document.querySelector("#description").innerHTML = response.data.weather[0].description;
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src",
+         `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
