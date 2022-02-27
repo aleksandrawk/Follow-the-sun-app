6888e727f3b56c72f9fb1fac75cedf9586936679
aleksandrawk <wiolettakulinska@tlen.pl>
2022-02-27 15:13:31 +0100

    get coords added

--- src/app.js
+++ src/app.js
@@ -25,7 +25,8 @@ currentTime.innerHTML = `${day} ${hours}:${minutes}`;
 console.log (currentTime);
 
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -53,6 +54,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "e8ae444df35b49d6068af39524f98513";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemp(response) {
     document.querySelector("h1").innerHTML = response.data.name;
     celsiusTemp = response.data.main.temp;
@@ -66,6 +74,7 @@ function showTemp(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+getForecast(response.data.coord);
 
   function searchCity(city) {
     let apiKey = "e8ae444df35b49d6068af39524f98513";
@@ -110,5 +119,3 @@ fLink.addEventListener ("click", showTempFarenheit);
 
 let cLink = document.querySelector ("#c-link");
 cLink.addEventListener ("click", showTempCelsius);
-
-displayForecast();
